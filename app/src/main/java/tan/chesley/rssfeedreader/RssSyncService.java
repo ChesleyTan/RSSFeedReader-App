bf82df60068952e18c303e5b1646bafb018323c9
ChesleyTan <ChesleyTan97@gmail.com>
2014-09-06 19:56:46 -0400

    Add countdown to notification
    Clear notification when the service is stopped

--- app/src/main/java/tan/chesley/rssfeedreader/RssSyncService.java
+++ app/src/main/java/tan/chesley/rssfeedreader/RssSyncService.java
@@ -20,6 +20,7 @@ public class RssSyncService extends Service {
     private static RssSyncService singleton;
     private final Service parent = this;
     private static boolean cancelled;
+    private int waited = 0;
 
     @Override
     public void onCreate () {
@@ -29,7 +30,7 @@ public class RssSyncService extends Service {
 
         // Create notification to show that this service is running
         Intent clickIntent = new Intent(getApplicationContext(), RSSFeed.class);
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        final NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         Notification notification;
 
         // Modify task stack to allow the user to return to the home screen when pressing the back button
@@ -39,7 +40,7 @@ public class RssSyncService extends Service {
         PendingIntent pendingClickIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
         // Build the notification
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
         builder.setSmallIcon(R.drawable.ic_launcher)
                .setContentTitle(getResources().getString(R.string.rssSyncService))
                .setContentText(getResources().getString(R.string.serviceIsCurrentlyRunning))
@@ -103,12 +104,21 @@ public class RssSyncService extends Service {
                     getRssFeedTask.execute(FEEDS);
 
                     try {
-                        Thread.sleep(interval);
+                        while (waited < interval) {
+                            builder.setContentText(getResources().getString(R.string.minutesToNextSync) + " " + Integer.toString((interval - waited) / 60000));
+                            notificationManager.notify(SYNC_STATUS_NOTIFICATION_ID, builder.build());
+                            waited += 60000;
+                            Thread.sleep(60000);
+                        }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                    } finally {
+                        waited = 0;
                     }
                 }
-
+                // Stop when syncing loop ends
+                cleanUp();
+                stopSelf();
             }
         }).start();
     }
@@ -128,7 +138,18 @@ public class RssSyncService extends Service {
 
     public void cancel() {
         cancelled = true;
+        cleanUp();
+    }
+
+    @Override
+    public void onDestroy () {
+        super.onDestroy();
+        cleanUp();
+    }
+
+    public void cleanUp() {
         singleton = null;
+        ((NotificationManager) getSystemService(NOTIFICATION_SERVICE)).cancel(SYNC_STATUS_NOTIFICATION_ID);
     }
 
     public static RssSyncService getInstance() {
